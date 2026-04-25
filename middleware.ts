bc5c5d7cf9f2b33da34dfbc19b541df1ad55c0a8
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-26 00:02:09 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,11 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-
-  // Headers de sécurité uniquement
   res.headers.set('X-Frame-Options', 'DENY')
   res.headers.set('X-Content-Type-Options', 'nosniff')
   res.headers.set('X-XSS-Protection', '1; mode=block')
   res.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
   res.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload')
-
   return res
 }
 
