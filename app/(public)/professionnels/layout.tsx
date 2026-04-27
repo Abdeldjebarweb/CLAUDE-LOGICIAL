e7508e3e26e63632f71f2d20c8aafdac9077a9b4
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:22:57 +0200

    Update description in layout.tsx

--- app/(public)/professionnels/layout.tsx
+++ app/(public)/professionnels/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Espace professionnels",
-  description: "Offres d'emploi pour etudiants algeriens de Bordeaux.",
+  description: "Offres emploi pour etudiants algeriens de Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
