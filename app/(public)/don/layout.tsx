e7f0b413392fb12d4a6060258e201d68fd4f680e
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:23:24 +0200

    Update layout.tsx

--- app/(public)/don/layout.tsx
+++ app/(public)/don/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Faire un don",
-  description: "Soutenez l'AEAB et aidez les etudiants algeriens de Bordeaux.",
+  description: "Soutenez lAEAB et aidez les etudiants algeriens de Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
