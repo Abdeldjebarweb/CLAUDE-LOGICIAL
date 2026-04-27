8d9a19454fec277156c1599d30c97c5b44740375
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:19:50 +0200

    Update layout.tsx

--- app/(public)/connexion/layout.tsx
+++ app/(public)/connexion/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Connexion',
-  description: 'Connectez-vous à votre espace membre AEAB.',
+  title: "Connexion",
+  description: "Connectez-vous a votre espace membre AEAB.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
