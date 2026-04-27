fadc2b99fdac326eb196dd7e76e1b5bbfa0e576c
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:23:44 +0200

    Fix description formatting in layout.tsx

--- app/(public)/contact/layout.tsx
+++ app/(public)/contact/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Contact",
-  description: "Contactez l'Association des Etudiants Algeriens de Bordeaux.",
+  description: "Contactez lAssociation des Etudiants Algeriens de Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
