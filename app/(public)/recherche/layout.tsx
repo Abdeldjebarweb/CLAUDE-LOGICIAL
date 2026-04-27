4030e10d1abf91704bfeebc28ce840ccd91d010a
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:21:09 +0200

    Change metadata strings to use double quotes

--- app/(public)/recherche/layout.tsx
+++ app/(public)/recherche/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Recherche',
-  description: 'Recherchez sur le site de l'AEAB Bordeaux.',
+  title: "Recherche",
+  description: "Recherchez sur le site de l'AEAB Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
