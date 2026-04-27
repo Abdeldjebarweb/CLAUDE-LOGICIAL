f0186b55b96130c50f494d039bcc20d91cc75154
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:22:30 +0200

    Fix description formatting in layout.tsx

--- app/(public)/recherche/layout.tsx
+++ app/(public)/recherche/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Recherche",
-  description: "Recherchez sur le site de l'AEAB Bordeaux.",
+  description: "Recherchez sur le site de lAEAB Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
