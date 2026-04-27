9e10ab607e19866443352d6d73e2f811c5ba131d
Ismail ABDELDJEBAR <ismail.abdeldjebar.web@gmail.com>
2026-04-27 22:24:56 +0200

    Update layout.tsx

--- app/(public)/adhesion/layout.tsx
+++ app/(public)/adhesion/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Adherer a l'AEAB",
-  description: "Rejoignez l'Association des Etudiants Algeriens de Bordeaux.",
+  title: "Adherer",
+  description: "Rejoignez lAssociation des Etudiants Algeriens de Bordeaux.",
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
